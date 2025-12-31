708e2ec68d4d3d4be9fa86dd12acfe55b55b05f6
adib150 <adibcpdomingos@gmail.com>
2025-12-30 21:31:46 -0300

    change diamond

--- data/scripts/weapons/scripts/diamond_arrow.lua
+++ data/scripts/weapons/scripts/diamond_arrow.lua
@@ -15,7 +15,7 @@ combat:setParameter(COMBAT_PARAM_CASTSOUND, SOUND_EFFECT_TYPE_DIST_ATK_BOW)
 combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
 function onGetFormulaValues(player, skill, attack, factor)
 	local distanceSkill = player:getEffectiveSkillLevel(SKILL_DISTANCE)
-	local min = (player:getLevel() / 5)
+	local min = (0.02 * factor) * distanceSkill * attack + (player:getLevel() / 5)
 	local max = (0.09 * factor) * distanceSkill * attack + (player:getLevel() / 5)
 	return -min, -max
 end
