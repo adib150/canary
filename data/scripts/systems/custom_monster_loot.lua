2a53c9b48928bfaeb1cd8d9b4e1cfe56d1b338b8
adib150 <adibcpdomingos@gmail.com>
2026-01-06 18:42:25 -0300

    add chance to drop tibia coin 0.001% from all mobs

--- data/scripts/systems/custom_monster_loot.lua
+++ data/scripts/systems/custom_monster_loot.lua
@@ -1,6 +1,6 @@
 -- Drops custom loot for all monsters
 local allLootConfig = {
-	{ id = 6526, chance = 100000, minCount = 1, maxCount = 10 }, -- Example of loot (100% chance)
+	{ id = 37317, chance = 1, minCount = 1, maxCount = 1 }, -- Chance to drop tibia coin 0.001%
 }
 
 -- Custom loot for specific monsters (this has the same usage options as normal monster loot)
